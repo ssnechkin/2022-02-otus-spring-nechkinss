9ae97a33b9f61d2772a5fd5e463c5a817b0f5a04
ssnechkin <ssnechkin@yandex.ru>
2022-07-28 14:11:15 +0300

    First commit

---
+++ homework_18_1/src/main/java/ru/otus/homework/controller/MenuController.java
@@ -0,0 +1,170 @@
+package ru.otus.homework.controller;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.acls.domain.BasePermission;
+import org.springframework.security.acls.domain.GrantedAuthoritySid;
+import org.springframework.security.acls.domain.ObjectIdentityImpl;
+import org.springframework.security.acls.model.Acl;
+import org.springframework.security.acls.model.Permission;
+import org.springframework.security.acls.model.Sid;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.otus.homework.domain.entity.Menu;
+import ru.otus.homework.domain.entity.security.RoleGrantedAuthority;
+import ru.otus.homework.domain.entity.security.UserDetail;
+import ru.otus.homework.domain.entity.security.acl.AclClass;
+import ru.otus.homework.domain.entity.security.acl.AclEntry;
+import ru.otus.homework.domain.entity.security.acl.AclObjectIdentity;
+import ru.otus.homework.domain.entity.security.acl.AclSid;
+import ru.otus.homework.dto.out.Content;
+import ru.otus.homework.dto.out.content.Button;
+import ru.otus.homework.dto.out.content.Link;
+import ru.otus.homework.dto.out.content.Notification;
+import ru.otus.homework.dto.out.content.TopRight;
+import ru.otus.homework.dto.out.enums.NotificationType;
+import ru.otus.homework.repository.MenuRepository;
+import ru.otus.homework.repository.security.acl.AclClassRepository;
+import ru.otus.homework.repository.security.acl.AclEntryRepository;
+import ru.otus.homework.repository.security.acl.AclObjectIdentityRepository;
+import ru.otus.homework.repository.security.acl.AclSidRepository;
+import ru.otus.homework.security.SecurityAclService;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
+
+@RestController
+public class MenuController {
+
+    private final MenuRepository menuRepository;
+    private final SecurityAclService securityAclService;
+    private final AclClassRepository aclClassRepository;
+    private final AclEntryRepository aclEntryRepository;
+    private final AclObjectIdentityRepository aclObjectIdentityRepository;
+    private final AclSidRepository aclSidRepository;
+
+    public MenuController(MenuRepository menuRepository, SecurityAclService securityAclService, AclObjectIdentityRepository aclObjectIdentityRepository, AclClassRepository aclClassRepository, AclEntryRepository aclEntryRepository, AclEntryRepository aclEntryRepository1, AclSidRepository aclSidRepository) {
+        this.menuRepository = menuRepository;
+        this.securityAclService = securityAclService;
+        this.aclObjectIdentityRepository = aclObjectIdentityRepository;
+        this.aclClassRepository = aclClassRepository;
+        this.aclEntryRepository = aclEntryRepository1;
+        this.aclSidRepository = aclSidRepository;
+        addAcl();
+    }
+
+    @GetMapping("/menu")
+    @HystrixCommand(commandKey="getFallKey", fallbackMethod="fallback")
+    public Content getMenu(@AuthenticationPrincipal UserDetail userDetail) {
+        //addMenu();
+        Content content = new Content();
+        TopRight topRight = new TopRight();
+        Button logout = new Button();
+        logout.setTitle("Выйти");
+        logout.setLink(new Link(HttpMethod.POST, "/logout"));
+        topRight.setButtons(List.of(logout));
+        topRight.setText(userDetail != null ? userDetail.getPublicName() : "");
+        content.setTopRight(topRight);
+        content.setButtons(getAssembledMenu(userDetail));
+        content.setManagement(List.of());
+        content.setPageName("Добро пожаловать в библиотеку книг");
+        content.setFields(List.of());
+        return content;
+    }
+
+    private Content fallback() {
+        return new Content().setNotifications(List.of(new Notification()
+                .setType(NotificationType.ERROR)
+                .setMessage("Серер перегружен.")
+        ));
+    }
+
+    private void addAcl() {
+        if (aclClassRepository.findAll().size() == 0) {
+            aclClassRepository.save(new AclClass(1, Menu.class.getName()));
+
+            aclSidRepository.save(new AclSid(1, false, "ROLE_ADMIN"));
+            aclSidRepository.save(new AclSid(2, false, "'ROLE_EDITOR'"));
+            aclSidRepository.save(new AclSid(3, false, "'ROLE_VISITOR'"));
+            aclSidRepository.save(new AclSid(4, false, "'visitor'"));
+            aclSidRepository.save(new AclSid(5, false, "'editor'"));
+
+            aclObjectIdentityRepository.save(new AclObjectIdentity(1, 1, "1", null, 2, true));
+            aclObjectIdentityRepository.save(new AclObjectIdentity(2, 1, "2", null, 2, true));
+            aclObjectIdentityRepository.save(new AclObjectIdentity(3, 1, "3", null, 2, true));
+
+            aclEntryRepository.save(new AclEntry(1, 1, 1, 1, 4, false, false, false));
+            aclEntryRepository.save(new AclEntry(2, 2, 2, 1, 3, false, false, false));
+            aclEntryRepository.save(new AclEntry(3, 3, 3, 1, 4, false, false, false));
+            aclEntryRepository.save(new AclEntry(4, 1, 4, 3, 1, false, false, false));
+            aclEntryRepository.save(new AclEntry(5, 1, 5, 2, 5, false, false, false));
+            aclEntryRepository.save(new AclEntry(6, 2, 6, 2, 6, false, false, false));
+            aclEntryRepository.save(new AclEntry(7, 3, 7, 2, 7, false, false, false));
+        }
+    }
+
+    private void addMenu() {
+        Menu author = menuRepository.findByLink("/author").get(0);
+        Menu genre = menuRepository.findByLink("/genre").get(0);
+        Menu book = menuRepository.findByLink("/book").get(0);
+        addMenuInAcl(author.getClass(), author.getId(), BasePermission.READ, "ROLE_ADMIN", true);
+        addMenuInAcl(author.getClass(), author.getId(), BasePermission.READ, "ROLE_EDITOR", true);
+        addMenuInAcl(genre.getClass(), genre.getId(), BasePermission.READ, "ROLE_ADMIN", true);
+        addMenuInAcl(genre.getClass(), genre.getId(), BasePermission.READ, "ROLE_EDITOR", true);
+        addMenuInAcl(book.getClass(), book.getId(), BasePermission.READ, "ROLE_ADMIN", true);
+        addMenuInAcl(book.getClass(), book.getId(), BasePermission.READ, "ROLE_EDITOR", true);
+        addMenuInAcl(book.getClass(), book.getId(), BasePermission.READ, "ROLE_VISITOR", true);
+    }
+
+    private void addMenuInAcl(Class<?> clas, Serializable objectId, Permission permission, String role, boolean granting) {
+        try {
+            securityAclService.addSecurityRoleRight(clas, objectId, permission, role, granting);
+        } catch (Exception ignore) {
+        }
+    }
+
+    private List<Button> getAssembledMenu(UserDetail userDetail) {
+        List<Button> buttons = new LinkedList<>();
+        for (Menu menu : getAllMenu()) {
+            try {
+                ObjectIdentityImpl oid = new ObjectIdentityImpl(Menu.class, menu.getId());
+                Acl acl = securityAclService.getJdbcMutableAclService().readAclById(oid);
+                List<Permission> permissions = List.of(BasePermission.READ);
+                List<Sid> sids = new ArrayList<>();
+                for (RoleGrantedAuthority role : userDetail.getRoles()) {
+                    sids.add(new GrantedAuthoritySid(role.getRole()));
+                }
+                try {
+                    if (acl.isGranted(permissions, sids, true) && acl.isSidLoaded(sids)) {
+                        buttons.add(new Button()
+                                .setPosition(menu.getPosition())
+                                .setTitle(menu.getTitle())
+                                .setAlt(true)
+                                .setLink(new Link()
+                                        .setValue(menu.getLink())
+                                        .setMethod(HttpMethod.valueOf(menu.getMethod()))
+                                )
+                        );
+                    }
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        Comparator<Button> comparator = (o1, o2) -> o1.getPosition() >= o2.getPosition() ? -1 : 0;
+        buttons.sort(comparator);
+        return buttons;
+    }
+
+    @PostFilter("hasPermission(filterObject, 'READ')")
+    private List<Menu> getAllMenu() {
+        return menuRepository.findAll();
+    }
+}
---
+++ homework_18_1/src/main/java/ru/otus/homework/controller/book/BookGenresController.java
@@ -0,0 +1,190 @@
+package ru.otus.homework.controller.book;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.*;
+import ru.otus.homework.domain.entity.book.Book;
+import ru.otus.homework.domain.entity.genre.Genre;
+import ru.otus.homework.dto.in.book.BookAuthorDto;
+import ru.otus.homework.dto.in.book.BookDto;
+import ru.otus.homework.dto.in.book.BookGenreDto;
+import ru.otus.homework.dto.out.Content;
+import ru.otus.homework.dto.out.content.*;
+import ru.otus.homework.dto.out.content.table.Row;
+import ru.otus.homework.dto.out.content.table.Table;
+import ru.otus.homework.dto.out.enums.FieldType;
+import ru.otus.homework.dto.out.enums.NotificationType;
+import ru.otus.homework.service.book.BookService;
+import ru.otus.homework.service.genre.GenreService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class BookGenresController {
+
+    private final BookService service;
+    private final GenreService genreService;
+
+    public BookGenresController(BookService service, GenreService genreService) {
+        this.service = service;
+        this.genreService = genreService;
+    }
+
+    @GetMapping("/book/{book_id}/genres")
+    @HystrixCommand(commandKey="getFallKey", fallbackMethod="fallbackBookId")
+    public Content list(@PathVariable("book_id") long bookId) {
+        Content content = new Content();
+        Button add = new Button();
+        add.setTitle("Добавить жанр");
+        add.setLink(new Link(HttpMethod.GET, "/book/" + bookId + "/genres/add"));
+        content.setPageName("Жанры у книги");
+        content.setManagement(List.of(add));
+        content.setTable(getTableAll(bookId));
+        return content;
+    }
+
+    @GetMapping("/book/{book_id}/genres/{id}")
+    @HystrixCommand(commandKey="getFallKey", fallbackMethod="fallbackIdBookId")
+    public Content view(@PathVariable("id") long id,
+                        @PathVariable("book_id") long bookId) {
+        return getContentView(bookId, id);
+    }
+
+    @GetMapping("/book/{book_id}/genres/add")
+    @HystrixCommand(commandKey="getFallKey", fallbackMethod="fallbackBookId")
+    public Content add(@PathVariable("book_id") long bookId) {
+        Content content = new Content();
+        Form form = new Form();
+        Button add = new Button();
+        Field name = new Field();
+        content.setPageName("Жанр у книги - добавление");
+        add.setLink(new Link(HttpMethod.POST, "/book/" + bookId + "/genres"));
+        add.setTitle("Добавить");
+        content.setManagement(List.of(add));
+        name.setType(FieldType.SELECT);
+        name.setLabel("Жанр");
+        name.setName("genre");
+        List<ValueItem> valueItems = new ArrayList<>();
+        for (Genre genre : genreService.getAll()) {
+            valueItems.add(new ValueItem(genre.getId(), genre.getName()));
+        }
+        name.setValues(valueItems);
+        form.setFields(List.of(name));
+        content.setForm(form);
+        return content;
+    }
+
+    @PostMapping("/book/{book_id}/genres")
+    @HystrixCommand(commandKey="getFallKey", fallbackMethod="fallbackBookGenreDtoBookId")
+    public Content create(@RequestBody BookGenreDto bookGenreDto,
+                          @PathVariable("book_id") long bookId) {
+        Notification notification = new Notification();
+        Content content;
+        Book book = service.getById(bookId);
+        Genre genre = genreService.getById(bookGenreDto.getGenre());
+        service.addGenre(book, genre);
+        content = getContentView(bookId, genre.getId());
+        notification.setType(NotificationType.INFO);
+        notification.setMessage("Жанр добавлен к книге");
+        content.setNotifications(List.of(notification));
+        return content;
+    }
+
+    @DeleteMapping("/book/{book_id}/genres/{id}")
+    @HystrixCommand(commandKey="getFallKey", fallbackMethod="fallbackBookIdId")
+    public Content delete(@PathVariable("book_id") long bookId,
+                          @PathVariable("id") long id) {
+        Notification notification = new Notification();
+        Content content = new Content();
+        Book book = service.getById(bookId);
+        Genre genre = genreService.getById(id);
+        boolean result = service.deleteGenre(book, genre);
+        Button add = new Button();
+        content.setPageName("Жанры у книги");
+        add.setTitle("Добавить");
+        add.setLink(new Link(HttpMethod.GET, "/book/" + bookId + "/genres/add"));
+        content.setManagement(List.of(add));
+        content.setTable(getTableAll(bookId));
+        if (result) {
+            notification.setType(NotificationType.INFO);
+            notification.setMessage("Жанр удален у книги");
+        } else {
+            notification.setType(NotificationType.WARNING);
+            notification.setMessage("Не удалось удалить. Жанр отсутствует у книги");
+        }
+        content.setNotifications(List.of(notification));
+        return content;
+    }
+
+    private Content fallback() {
+        return new Content().setNotifications(List.of(new Notification()
+                .setType(NotificationType.ERROR)
+                .setMessage("Серер перегружен.")
+        ));
+    }
+
+    private Content fallbackId(long id) {
+        return fallback();
+    }
+
+    private Content fallbackBookId(long bookId) {
+        return fallback();
+    }
+
+    private Content fallbackBookIdId(long bookId, long id) {
+        return fallback();
+    }
+
+    private Content fallbackBookGenreDtoBookId(BookGenreDto bookGenreDto, long bookId) {
+        return fallback();
+    }
+
+    private Content fallbackIdBookId(long id, long bookId) {
+        return fallback();
+    }
+
+    private Content fallbackIdBookDto(long id, BookDto bookDto) {
+        return fallback();
+    }
+
+    private Content fallbackBookDto(BookDto bookDto) {
+        return fallback();
+    }
+
+    private Table getTableAll(long bookId) {
+        Table table = new Table();
+        table.setLabels(List.of("Наименование", "Описание"));
+        List<Row> rows = new ArrayList<>();
+        for (Genre genre : service.getById(bookId).getGenres()) {
+            Row row = new Row();
+            row.setLink(new Link(HttpMethod.GET, "/book/" + bookId + "/genres/" + genre.getId()));
+            row.setColumns(List.of(genre.getName(), genre.getDescription() == null ? "" : genre.getDescription()));
+            rows.add(row);
+        }
+        table.setRows(rows);
+        return table;
+    }
+
+    private Content getContentView(long bookId, long id) {
+        Genre genre = genreService.getById(id);
+        Content content = new Content();
+        Button delete = new Button();
+        Field name = new Field(), description = new Field();
+        content.setPageName("Жанр у книги");
+        delete.setPosition(2);
+        delete.setTitle("Удалить");
+        delete.setLink(new Link(HttpMethod.DELETE, "/book/" + bookId + "/genres/" + genre.getId()));
+        content.setManagement(List.of(delete));
+        name.setType(FieldType.INPUT);
+        name.setLabel("Наименование");
+        name.setName("name");
+        name.setValue(genre.getName());
+        description.setType(FieldType.INPUT);
+        description.setLabel("Описание");
+        description.setName("description");
+        description.setValue(genre.getDescription());
+        content.setFields(List.of(name, description));
+        return content;
+    }
+}
